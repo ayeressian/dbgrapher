855dd9321787eaf29a1c69567e374ea15b4acab3
Ara Yeressian <yeressian@tuta.io>
2020-02-05 19:49:00 +0400

    init

---
+++ src/components/side-panel.ts
@@ -0,0 +1,48 @@
+import { LitElement, html, customElement, css, unsafeCSS } from 'lit-element';
+import createIconImg from '../../asset/icon_create_table_48x48.png';
+import relationIconImg from '../../asset/icon_create_relation_48x48.png';
+
+@customElement('dbg-side-panel')
+class SidePanel extends LitElement {
+  static get styles() {
+    return css`
+      .left_toolbar {
+        padding: 0;
+        margin: 0;
+      }
+      
+      .left_toolbar .action {
+        width: 60px;
+        height: 60px;
+        list-style-type: none;
+        background-repeat: no-repeat;
+        background-position: center;
+      }
+      
+      .left_toolbar .action:hover {
+        background-color: #dddddd;
+      }
+      
+      .left_toolbar .action.active {
+        background-color: #d68080;
+      }
+      
+      .left_toolbar .action.create_table {
+        background-image: url(${unsafeCSS(createIconImg)});
+      }
+      
+      .left_toolbar .action.create_relation {
+        background-image: url(${unsafeCSS(relationIconImg)});
+      }    
+    `;
+  }
+
+  render(){
+    return html`
+      <ul class="left_toolbar">
+        <li class="action create_table" title="Create Table"></li>
+        <li class="action create_relation" title="Create Relation"></li>
+      </ul>
+    `;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    contentBase: 'dist',
+    port: 9999
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: 'src/index.html',
+      },
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /(?<!\.d)\.ts?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.d\.ts$/,
+        loader: 'ignore-loader'
+      },
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};

06282c144cc901a894e3630730e18e56313fc2a6
Ara Yeressian <ayeressian2@gmail.com>
2020-03-09 22:53:15 +0400

    fix form validation

--- src/components/table-dialog/columns.ts
+++ src/components/table-dialog/columns.ts
@@ -98,7 +98,7 @@ export default class extends LitElement {
   }
 
   validate() {
-    return this.form!.checkValidity();
+    return this.form!.reportValidity();
   }
   
   render(): TemplateResult {

7119032d4603208847ed060caffcddf26bc506de
Ara Yeressian <ayeressian2@gmail.com>
2020-05-13 10:51:26 +0400

    one drive load file

--- src/drive/one-drive.ts
+++ src/drive/one-drive.ts
@@ -1,4 +1,8 @@
 import env from '../../env.json';
+import store from '../store/store';
+import { actions as schemaAction } from '../store/slices/schema';
+import { actions as setSchemaAction } from '../store/slices/load-schema';
+import { actions as fileOpenChooserAction } from "../store/slices/file-open-chooser-dialog";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 type Options = {
@@ -22,8 +26,16 @@ declare global {
 export function picker(): void {
   const options = {
     clientId: env.oneDrive.clientId,
+    action: 'download',
     success: (files: any): void => {
-      console.log(files);
+      const file = files.value[0];
+      fetch(file['@microsoft.graph.downloadUrl']).then((result) => {
+        return result.json();
+      }).then(result => {
+        store.dispatch(schemaAction.initiate(result));
+        store.dispatch(fileOpenChooserAction.close());
+        store.dispatch(setSchemaAction.load());
+      });
     }
   };
   OneDrive.open(options);

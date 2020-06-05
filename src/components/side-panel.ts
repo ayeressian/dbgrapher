d54fb0bd34ef6efaaec368d2e21477b4ab0fae46
Ara Yeressian <yeressian@tuta.io>
2020-06-05 10:48:13 +0400

    added new actions for other relation types in sidebar

--- src/components/side-panel.ts
+++ src/components/side-panel.ts
@@ -2,7 +2,10 @@ import { LitElement, html, customElement, css, unsafeCSS, CSSResult, TemplateRes
 import { actions as dbViewerModeAction } from '../store/slices/db-viewer-mode';
 import store from '../store/store';
 import createIconImg from '../../asset/table.svg';
-import relationIconImg from '../../asset/relation.svg';
+import relationOneToManyIcon from '../../asset/relation-one-to-many.svg';
+import relationOneToOneIcon from '../../asset/relation-one-to-one.svg';
+import relationZeroToOneIcon from '../../asset/relation-zero-to-one.svg';
+import relationZeroToManyIcon from '../../asset/relation-zero-to-many.svg';
 import { classMap } from 'lit-html/directives/class-map';
 import { subscribe } from '../subscribe-store';
 import { IDbViewerMode } from '../store/slices/db-viewer-mode-interface';
@@ -41,12 +44,23 @@ export default class extends LitElement {
       }
       
       .left_toolbar .action.create_table {
-
         background-image: url(${unsafeCSS(createIconImg)});
       }
       
-      .left_toolbar .action.create_relation {
-        background-image: url(${unsafeCSS(relationIconImg)});
+      .left_toolbar .action.create_relation_one_to_many {
+        background-image: url(${unsafeCSS(relationOneToManyIcon)});
+      }
+
+      .left_toolbar .action.create_relation_zero_to_many {
+        background-image: url(${unsafeCSS(relationZeroToManyIcon)});
+      }
+
+      .left_toolbar .action.create_relation_one_to_one {
+        background-image: url(${unsafeCSS(relationOneToOneIcon)});
+      }
+
+      .left_toolbar .action.create_relation_zero_to_one {
+        background-image: url(${unsafeCSS(relationZeroToOneIcon)});
       }
 
       .safari-height {
@@ -62,7 +76,10 @@ export default class extends LitElement {
     return html`
       <ul class="left_toolbar ${classMap({'safari-height': isSafari})}">
         <li class="action create_table ${classMap({active: this.#createActive})}" title="Create Table" @click="${this.#create}"></li>
-        <li class="action create_relation ${classMap({active: this.#relationActive})}" title="Create Relation" @click="${this.#relation}"></li>
+        <li class="action create_relation_one_to_many ${classMap({active: this.#relationActive})}" title="Create Relation" @click="${this.#relationOneToMany}"></li>
+        <li class="action create_relation_zero_to_many ${classMap({active: this.#relationActive})}" title="Create Relation" @click="${this.#relationZeroToMany}"></li>
+        <li class="action create_relation_one_to_one ${classMap({active: this.#relationActive})}" title="Create Relation" @click="${this.#relationOneToOne}"></li>
+        <li class="action create_relation_zero_to_one ${classMap({active: this.#relationActive})}" title="Create Relation" @click="${this.#relationZeroToOne}"></li>
       </ul>
     `;
   }
@@ -76,7 +93,7 @@ export default class extends LitElement {
           this.#createActive = true;
           this.#relationActive = false;
           break;
-        case IDbViewerMode.Relation:
+        case IDbViewerMode.RelationOneToMany:
           this.#createActive = false;
           this.#relationActive = true;
           break;
@@ -97,11 +114,23 @@ export default class extends LitElement {
     }
   }
 
-  #relation = (): void => {
-    if (store.getState().dbViewerMode === IDbViewerMode.Relation) {
+  #relationOneToMany = (): void => {
+    if (store.getState().dbViewerMode === IDbViewerMode.RelationOneToMany) {
       store.dispatch(dbViewerModeAction.none());
     } else {
       store.dispatch(dbViewerModeAction.relationMode());
     }
+  }
+
+  #relationZeroToMany = (): void => {
+    //TODO
+  }
+
+  #relationOneToOne = (): void => {
+    //TODO
+  }
+
+  #relationZeroToOne = (): void => {
+    //TODO
   }
 }

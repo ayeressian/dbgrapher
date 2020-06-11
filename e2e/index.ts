07671956ef96ef96c46014476305d276a97a1419
Ara Yeressian <yeressian@tuta.io>
2020-06-11 17:51:40 +0400

    add more e2e tests

--- e2e/index.ts
+++ e2e/index.ts
@@ -38,5 +38,25 @@ describe('New file', () => {
       const attr = await page.getAttribute('dbg-app dbg-welcome-dialog dbg-dialog', 'show');
       expect(attr).toBe(null);
     });
+
+    describe('when cliking on new "create new table" side panel action', () => {
+      beforeEach(async () => {
+        await page.click('dbg-app dbg-side-panel .create_table');
+      });
+      it ('should be activated', async () => {
+        const elem = await page.$('dbg-app dbg-side-panel .create_table.active');
+        expect(elem).not.toBe(null);
+      });
+
+      describe('when clicking on viewer', () => {
+        beforeEach(async () => {
+          await page.click('dbg-app dbg-db-viewer db-viewer');
+        });
+        it('should open create table dialog', async () => {
+          const attr = await page.getAttribute('dbg-app dbg-table-dialog dbg-dialog', 'show');
+          expect(attr).toBe('');
+        });
+      });
+    });
   });
 });

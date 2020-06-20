54e46b5937a9a4bceb30a461211cf335ffd88f99
Ara Yeressian <ayeressian2@gmail.com>
2020-06-20 10:52:41 +0400

    Updated favicon. Added hint for saving when in google drive mode.

--- src/components/hint.ts
+++ src/components/hint.ts
@@ -1,6 +1,10 @@
 import { LitElement, customElement, css, CSSResult, TemplateResult, html } from 'lit-element';
 import { IDbViewerMode } from '../store/slices/db-viewer-mode-interface';
 import store from '../store/store';
+import { isMac } from '../util';
+import { State as FileOpenChooserDialogState } from '../store/slices/file-open-chooser-dialog';
+
+const DISPLAY_TIMER = 5000;
 
 @customElement('dbg-hint')
 export default class extends LitElement {
@@ -17,17 +21,30 @@ export default class extends LitElement {
         color: #ddd;
         font-size: 1.2em;
         padding: 5px;
+        padding-left: 8px;
+        padding-right: 8px;
         border-radius: 2px;
       }
     `;
   }
 
   render(): TemplateResult {
-    return this.#text? html`
+    return this.#text ? html`
       <div>
         ${this.#text}
       </div>
     ` : html``;
+  }
+
+  #showTimedMessage = (message: string): void => {
+    setTimeout(() => {
+      if (this.#text === message) {
+        this.#text = '';
+        this.requestUpdate();
+      }
+    }, DISPLAY_TIMER);
+    this.#text = message;
+    this.requestUpdate();
   }
 
   connectedCallback(): void {
@@ -43,6 +60,21 @@ export default class extends LitElement {
         this.#text = 'Click on the first table to create the relation from and then click on the second table to create the relation to';
       }
       this.requestUpdate();
+    });
+
+    window.addEventListener('keydown', (event) => {
+      const state = store.getState();
+      if (((isMac && event.metaKey) ||
+        (!isMac && event.ctrlKey)) &&
+        event.key === 's' &&
+        state.googleDriveKey != null &&
+        !state.dialog.tableDialog.open &&
+        !state.dialog.welcomeDialog &&
+        !state.dialog.aboutDialog &&
+        state.dialog.fileOpenChooserDialog === FileOpenChooserDialogState.Close) {
+        this.#showTimedMessage('Your changes will be automatically saved to Google drive');
+      }
+      event.preventDefault();
     });
   }
 }

b7eee1d67e639603134200d27eaf7b386b0ce2a8
Ara Yeressian <ayeressian2@gmail.com>
2020-07-10 20:17:23 +0400

    fix e2e tests

--- e2e/new-file-table-dialog.ts
+++ e2e/new-file-table-dialog.ts
@@ -28,38 +28,47 @@ describe('New file', () => {
     expect(await page.title()).toBe('DBGrapher');
   });
 
-  it('welcome dialog should be visible', async () => {
-    const attr = await page.getAttribute('dbg-app dbg-welcome-dialog dbg-dialog', 'show');
+  it('should show cloud provider dialog', async () => {
+    const attr = await page.getAttribute('dbg-app dbg-cloud-provider-dialog dbg-dialog', 'show');
     expect(attr).toBe('');
   });
 
-  describe('when clicked on new file', () => {
+  describe('when none has selected from cloud provider dialog', () => {
     beforeEach(async () => {
-      await page.click('dbg-app dbg-welcome-dialog dbg-dialog .new-file');
+      await page.click('dbg-app dbg-cloud-provider-dialog dbg-dialog .no-drive');
     });
-    it ('should close the welcome dialog', async () => {
-      const attr = await page.getAttribute('dbg-app dbg-welcome-dialog dbg-dialog', 'show');
-      expect(attr).toBe(null);
+    it('should show new open file dialog', async () => {
+      const attr = await page.getAttribute('dbg-app dbg-new-open-dialog dbg-dialog', 'show');
+      expect(attr).toBe('');
     });
-
-    describe('when cliking on new "create new table" side panel action', () => {
+    describe('when clicked on new file', () => {
       beforeEach(async () => {
-        await page.click('dbg-app dbg-side-panel .create_table');
+        await page.click('dbg-app dbg-new-open-dialog dbg-dialog .new-file');
       });
-      it ('should be activated', async () => {
-        const elem = await page.$('dbg-app dbg-side-panel .create_table.active');
-        expect(elem).not.toBe(null);
+      it ('should close the new open file dialog', async () => {
+        const attr = await page.getAttribute('dbg-app dbg-new-open-dialog dbg-dialog', 'show');
+        expect(attr).toBe(null);
       });
-
-      describe('when clicking on viewer', () => {
+  
+      describe('when cliking on new "create new table" side panel action', () => {
         beforeEach(async () => {
-          await page.click('dbg-app dbg-db-viewer db-viewer');
+          await page.click('dbg-app dbg-side-panel .create_table');
         });
-        it('should open create table dialog', async () => {
-          const attr = await page.getAttribute('dbg-app dbg-table-dialog dbg-dialog', 'show');
-          expect(attr).toBe('');
+        it ('should be activated', async () => {
+          const elem = await page.$('dbg-app dbg-side-panel .create_table.active');
+          expect(elem).not.toBe(null);
         });
-        createTableDialog(passData as {page: Page});
+  
+        describe('when clicking on viewer', () => {
+          beforeEach(async () => {
+            await page.click('dbg-app dbg-db-viewer db-viewer');
+          });
+          it('should open create table dialog', async () => {
+            const attr = await page.getAttribute('dbg-app dbg-table-dialog dbg-dialog', 'show');
+            expect(attr).toBe('');
+          });
+          createTableDialog(passData as {page: Page});
+        });
       });
     });
   });
